ea54c0f7cf1631403fe870d8057c26b2d4f59746
max_yang <1362459517@qq.com>
2022-10-03 18:27:52 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm ~/.tmux.conf
 rm ~/.tmux.conf.local
 
 ln -s $path/zsh/.zshrc ~/.zshrc
+ln -s $path/zsh/.zsh ~/.zsh
 ln -s $path/zsh/.oh-my-zsh ~/.oh-my-zsh
 ln -s $path/tmux/.tmux.conf ~/.tmux.conf
 ln -s $path/tmux/.tmux.conf.local ~/.tmux.conf.local
---
+++ zsh/.zsh
@@ -0,0 +1,1 @@
+/home/addr/.config/dotfiles/zsh
