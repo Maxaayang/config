626f566658ce16d370050f651d43d980a0139e13
max_yang <1362459517@qq.com>
2022-10-03 17:34:49 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+path=$(pwd)
+
 rm -rf ~/.oh-my-zsh
 rm ~/.zshrc
 rm ~/.tmux.conf
 rm ~/.tmux.conf.local
 
-ln -s ./zsh/.zshrc ~/.zshrc
-ln -s ./zsh/.oh-my-zsh ~/.oh-my-zsh
-ln -s ./tmux/.tmux.conf ~/.tmux.conf
-ln -s ./tmux/.tmux.conf.local ~/.tmux.conf.local
+ln -s $pwd/zsh/.zshrc ~/.zshrc
+ln -s $pwd/zsh/.oh-my-zsh ~/.oh-my-zsh
+ln -s $pwd/tmux/.tmux.conf ~/.tmux.conf
+ln -s $pwd/tmux/.tmux.conf.local ~/.tmux.conf.local
 
 source ~/.zshrc
