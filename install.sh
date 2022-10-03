748d45a1fedc527e8fd92243372f9e346322d311
max_yang <1362459517@qq.com>
2022-10-03 19:43:40 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 path=$(pwd)
 
-# rm -rf ~/.oh-my-zsh
 rm ~/.zshrc
 rm ~/.tmux.conf
 rm ~/.tmux.conf.local
 
 ln -s $path/zsh/.zshrc ~/.zshrc
-# ln -s $path/zsh/.zsh ~/.zsh
-# ln -s $path/zsh/.oh-my-zsh ~/.oh-my-zsh
 ln -s $path/tmux/.tmux.conf ~/.tmux.conf
 ln -s $path/tmux/.tmux.conf.local ~/.tmux.conf.local
+
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# chsh -s /bin/zsh
